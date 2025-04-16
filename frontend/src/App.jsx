f1c93386479fd85103938188e9453d85cbc6f3af
wizdeX <surjith.ap007@gmail.com>
2025-04-16 19:57:56 +0530

    Fully Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,19 @@ import './css/App.css';
 import Favorites from "./pages/Favorites"
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
 
 function App() {
   return (
-    <div><NavBar/>
+    <MovieProvider>
+      <NavBar/>
     <main className="main-content">
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/favorites" element={<Favorites/>}/>
       </Routes>
     </main>
-    </div>
+    </MovieProvider>
   );
 }
 
